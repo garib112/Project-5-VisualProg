ffbce74c43c45b996287d58d36b60a936c477a99
Alberto <garib112@mail.chapman.edu>
2018-03-28 14:45:27 -0700

    first commit

---
+++ Project05_agaribay/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Project05_agaribay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(45, 299);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(308, 139);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Current Student";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(235, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(192, 264);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add Record";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(45, 124);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Class Standing";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(45, 189);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 28);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "First Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Last Name:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 20);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Standing:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Project 05 - Student Manager";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Project05_agaribay/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Project05_agaribay
+{
+    public partial class Form1 : Form
+    {
+        BindingList<Student> studentList = new BindingList<Student>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set the GUI listbox to use the studentList as a data source
+            recordListbox.DataSource = studentList;
+            recordListbox.DisplayMember = "displayName";
+
+            loadRecords();
+        }
+
+        /*
+         * addButton_Click
+         * Triggers when pressing the "Add Record" button.
+         * Checks user input, and then creates a new Student object to add to the studentList.
+         * 
+         * @ref checkInputs
+         */
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (!checkInputs())
+                return;
+
+            studentList.Add(new Student(fnameInput.Text, lnameInput.Text, standingInput.SelectedItem.ToString()));
+        }
+
+        /*
+         * checkInputs
+         * Checks the input fields for valid entry data
+         * 
+         * @return bool Whether or not the inputs all checkout
+         * 
+         * TODO: Modify the if statements to check for valid inputs
+         */
+        private bool checkInputs()
+        {
+            bool pass = true;
+
+            if (fnameInput.Text == "")
+                pass = false;
+
+            if (lnameInput.Text == "")
+                pass = false;
+
+            if (standingInput.SelectedItem == null)
+                pass = false;
+
+            if (!pass) MessageBox.Show("You have entered invalid inputs.");
+
+            return pass;
+        }
+
+        /*
+         * recordListbox_SelectedIndexChanged
+         * Triggers when changing the select item in the GUI listbox.
+         * Gets the index of the selected item and uses it to get the same index in
+         * the studentList.
+         */
+        private void recordListbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (recordListbox.SelectedIndex > -1)
+            {
+                fnameOutput.Text = studentList[recordListbox.SelectedIndex].fname;
+                lnameOutput.Text = studentList[recordListbox.SelectedIndex].lname;
+                standingOutput.Text = studentList[recordListbox.SelectedIndex].standing;
+            }
+        }
+
+        /*
+         * Form1_FormClosing
+         * Triggers when closing the application window.
+         * Calls the saveRecords method.
+         */
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            saveRecords();
+        }
+
+        /*
+         * loadRecords
+         * Loads a previously saved record of student data into the studentList variable
+         */
+        private void loadRecords()
+        {
+            // TODO: Implement loadRecords()
+        }
+
+        /*
+         * saveRecords
+         * Saves the records from the studentList variable to a file
+         */
+        private void saveRecords()
+        {
+            // TODO: Implement saveRecords()
+        }
+    }
+
+    class Student
+    {
+        public string fname;
+        public string lname;
+        public string standing;
+        public string displayName
+        {
+            get
+            {
+                return lname + ", " + fname;
+            }
+        }
+
+        public Student(string FirstName, string LastName, string Standing)
+        {
+            this.fname = FirstName;
+            this.lname = LastName;
+            this.standing = Standing;
+        }
+    }
+}
+
